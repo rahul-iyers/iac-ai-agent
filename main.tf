bc7729c6d6030c3b3b042f484045de78b95bcfb1
Rahul Iyer <rahul1221liyer@gmail.com>
2025-06-17 14:41:56 -0400

    adding main tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "unsecure_bucket" {
+  bucket = "my-public-bucket-123456"
+  acl    = "public-read"
+}
+
+resource "aws_security_group" "open_sg" {
+  name        = "open_sg"
+  description = "Allow all traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
